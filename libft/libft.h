ff42a0a9ca2fca948a63a155ca08954fa376448b
sarafedezlo <129091038+sarafedezlo@users.noreply.github.com>
2023-05-12 20:31:45 +0000

    encallado en el memmove

--- libft/libft.h
+++ libft/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stddef.h>
+# include <ctype.h>
 
 int		ft_isalnum(int c);
 size_t	ft_strlen(const char *str);
@@ -25,5 +26,10 @@ int		ft_isprint(int c);
 void	*ft_memset(void *r, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void    *memmove(void *dst, const void *src, size_t n);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+char    *strchr(const char *s, int c);
+char    *strrchr(const char *s, int c);
+int     strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
--- libft/main_memmove.c
+++ libft/main_memmove.c
@@ -5,13 +5,13 @@ int main () {
    char dest[] = "oldstringewew";
    const char src[]  = "newstrg";
 
+   printf("Before memmove dest = %s, src = %s\n", dest, src);
+   memmove(dest, src, 4);
+   printf("After memmove dest = %s, src = %s\n", dest, src);
+
    printf("Before memcpy dest = %s, src = %s\n", dest, src);
    memcpy(dest, src, 8);
    printf("After memcpy dest = %s, src = %s\n", dest, src);
-
-   printf("Before memmove dest = %s, src = %s\n", dest, src);
-   memmove(dest, src, 8);
-   printf("After memmove dest = %s, src = %s\n", dest, src);
 
    return(0);
 }
