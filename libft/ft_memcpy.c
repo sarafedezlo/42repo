47fece5876d7543b4738d3369fc0ca79895ef5d4
Sara Fernandez Lopez <sarferna@cbr6s3.42barcelona.com>
2023-05-15 10:17:37 +0200

    anteriores

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,20 +6,22 @@
 /*   By: sarferna <sarferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:21:17 by sarferna          #+#    #+#             */
-/*   Updated: 2023/05/09 15:57:42 by sarferna         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:38:33 by sarferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	size_t	i;
 
 	i = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
-		dst[i] = src[i];
+		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
 		i++;
 	}
 	return (dst);
