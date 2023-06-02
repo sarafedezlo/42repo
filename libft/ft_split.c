64f665e4ded0b9fe7baf7222ffd74bc0b8647a24
Sara Fernandez Lopez <sarferna@cbr5s1.42barcelona.com>
2023-06-02 13:49:57 +0200

    dsdfjh

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,12 @@
 /*   By: sarferna <sarferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 12:53:26 by sarferna          #+#    #+#             */
-/*   Updated: 2023/06/01 17:43:50 by sarferna         ###   ########.fr       */
+/*   Updated: 2023/06/02 13:47:27 by sarferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	count_word(char const *s, char c)
 {
@@ -19,7 +20,7 @@ size_t	count_word(char const *s, char c)
 
 	i = 0;
 	r = 0;
-	if (s[i] == c)
+	while (s[i] == c)
 		i++;
 	while (s[i])
 	{
@@ -30,6 +31,7 @@ size_t	count_word(char const *s, char c)
 	return (r);
 }
 
+
 char	**ft_split(char const *s, char c)
 {
 	char	**a;
@@ -38,29 +40,48 @@ char	**ft_split(char const *s, char c)
 	int		pi;
 
 	i = 0;
+	j = 0;
+	pi = 0;
 	if (!s)
 		return (NULL);
-	*a = malloc(count_word(s, c) + 1);
-	if (!*a)
+	a = (char **)malloc(count_word(s, c) + 1 * sizeof(char *));
+	if (!a)
 		return (NULL);
 	while (s[i])
 	{
-		if (((char *)s)[i] != c && s[i - 1] == c || i = 0 && s[i] != c)
+		if ((i == 0 && s[i] != c) || (s[i] != c && s[i - 1] == c))
 			pi = i;
 		if ((s[i] == c && s[i + 1] != c) && pi != 0)
 		{
-			a[j] = ft_substr(*s, pi - 1, i - pi);
+			a[j] = ft_substr(s, pi, i - pi);
 			if (!a[j])
 			{
 				while (j > 0)
-					free (a[--j]);
-				free (a);
+					free(a[--j]);
+				free(a);
 				return (NULL);
 			}
 			j++;
 			pi = 0;
 		}
-		i++
+		i++;
 	}
+	a[j] = NULL;
 	return (a);
 }
+
+// int	main(void)
+// {
+//  	char *s = "    hola      que   tal    wena    teta     ";
+
+// 	char	**y;
+// 	int	i;
+
+// 	i = 0;
+// 	y = ft_split(s, ' ');
+// 	while (y[i])
+// 	{
+// 		printf("%s\n", y[i]);
+// 		i++;
+// 	}
+// }
